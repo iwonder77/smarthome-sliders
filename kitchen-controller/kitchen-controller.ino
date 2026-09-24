6e482759074569d0453a80970f766baf2d1e39c9
Isai Sanchez <isanchez@thanksgivingpoint.org>
2026-09-24 14:09:57 -0600

    feat: added button confirmation detection logic, will place into its own class later

--- kitchen-controller/kitchen-controller.ino
+++ kitchen-controller/kitchen-controller.ino
@@ -4,10 +4,35 @@
 Slider slider1(1, config::SLIDER1_PINS);
 Slider slider2(2, config::SLIDER2_PINS);
 
+bool stable = false;
+bool last_raw = false;
+uint32_t last_button_press_timestamp = 0;
+
+bool buttonPressed(uint32_t now) {
+  bool raw = digitalRead(config::BUTTON_PIN) == LOW;
+
+  if (raw != last_raw){
+    last_raw = raw;
+    last_button_press_timestamp = now;
+    return false;
+  }
+
+  if (raw == stable) {
+    return false;
+  };
+  if (now - last_button_press_timestamp < config::BUTTON_DEBOUNCE_MS) {
+    return false;
+  }
+
+  stable = raw;
+  return stable;
+}
+
 void setup() {
   Serial.begin(115200);
   while (!Serial && millis() < 3000) {}  // USB CDC needs time to enumerate
 
+  pinMode(config::BUTTON_PIN, INPUT);
   slider1.init();
   slider2.init();
 }
@@ -16,10 +41,8 @@ void loop() {
   uint32_t now = millis();
   slider1.update(now);
   slider2.update(now);
-  if (slider1.reedsJustChanged()) {
+  if (buttonPressed(now)) {
     slider1.logSlider();
-  }
-  if (slider2.reedsJustChanged()) {
     slider2.logSlider();
   }
 }
