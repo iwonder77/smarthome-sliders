968edd512044a7ef79ada062bab6ddb57ed8dc07
Isai Sanchez <isanchez@thanksgivingpoint.org>
2026-09-24 13:23:13 -0600

    fix: each slider will have its own array of reed input pins

--- kitchen-controller/src/Config.h
+++ kitchen-controller/src/Config.h
@@ -15,7 +15,6 @@ constexpr uint32_t BUTTON_DEBOUNCE_MS = 20;
 
 constexpr uint8_t NUM_SLIDERS = 2;
 constexpr uint8_t NUM_REEDS_PER_SLIDER = 3;
-constexpr uint8_t NUM_REEDS = NUM_SLIDERS * NUM_REEDS_PER_SLIDER;
 constexpr uint8_t SLIDER1_PINS[NUM_REEDS_PER_SLIDER] = {42, 47, 48};
 constexpr uint8_t SLIDER2_PINS[NUM_REEDS_PER_SLIDER] = {39, 40, 41};
 constexpr uint32_t REED_DEBOUNCE_MS = 40;
