1a1c8467c2cb053e0fe42957f9db136f8b13ec6c
Isai Sanchez <isanchez@thanksgivingpoint.org>
2026-09-24 10:46:12 -0600

    feat: 2 arrays of pins for each sliders' reed switch GPIO pins

--- kitchen-controller/src/Config.h
+++ kitchen-controller/src/Config.h
@@ -16,7 +16,7 @@ constexpr uint32_t BUTTON_DEBOUNCE_MS = 20;
 constexpr uint8_t NUM_SLIDERS = 2;
 constexpr uint8_t NUM_REEDS_PER_SLIDER = 3;
 constexpr uint8_t NUM_REEDS = NUM_SLIDERS * NUM_REEDS_PER_SLIDER;
-constexpr uint8_t REED_PINS[NUM_SLIDERS][NUM_REEDS_PER_SLIDER] = {{42, 47, 48},
-                                                                  {39, 40, 41}};
+constexpr uint8_t SLIDER1_PINS[NUM_REEDS_PER_SLIDER] = {42, 47, 48};
+constexpr uint8_t SLIDER2_PINS[NUM_REEDS_PER_SLIDER] = {39, 40, 41};
 constexpr uint32_t REED_DEBOUNCE_MS = 40;
 } // namespace config
