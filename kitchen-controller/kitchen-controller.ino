df38559efee9e22f24b60b9094454df3cdd644a1
Isai Sanchez <isanchez@thanksgivingpoint.org>
2026-09-24 13:22:49 -0600

    feat: replaced reed switch object logic with slider objects

--- kitchen-controller/kitchen-controller.ino
+++ kitchen-controller/kitchen-controller.ino
@@ -1,8 +1,8 @@
 #include "src/Config.h"
 #include "src/Slider.h"
 
-Slider slider1(config::SLIDER1_PINS);
-Slider slider2(config::SLIDER2_PINS);
+Slider slider1(1, config::SLIDER1_PINS);
+Slider slider2(2, config::SLIDER2_PINS);
 
 void setup() {
   Serial.begin(115200);
@@ -16,4 +16,10 @@ void loop() {
   uint32_t now = millis();
   slider1.update(now);
   slider2.update(now);
+  if (slider1.reedsJustChanged()) {
+    slider1.logSlider();
+  }
+  if (slider2.reedsJustChanged()) {
+    slider2.logSlider();
+  }
 }
